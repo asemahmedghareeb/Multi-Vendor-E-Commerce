7dc01ccbf40386c58aa602ea5e09d58af0737c63
Baianat <baianat@Asem-iMac.local>
2025-12-22 12:43:55 +0200

    new

--- src/modules/app/orders/dto/inputs/create-order.input.ts
+++ src/modules/app/orders/dto/inputs/create-order.input.ts
@@ -1,12 +1,12 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsJSON, IsNotEmpty, IsString } from 'class-validator';
 import { PaymentGatewaysEnum } from 'src/modules/core/payment/enums/payment-gateways.enum';
 
 @InputType()
 export class CreateOrderInput {
   @Field()
   @IsNotEmpty()
-  // In a real app, you might use a nested object, but JSON string is fine for now
+  // @IsJSON()
   @IsString() 
   shippingAddress: string; 
 
